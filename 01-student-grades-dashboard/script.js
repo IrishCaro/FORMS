1c9d45b1f219b54b3d570489ca09c355f2a85b83
IrishCaro <irishcaro026@gmail.com>
2025-08-05 06:30:37 +0800

    starter pack

--- 01-student-grades-dashboard/script.js
+++ 01-student-grades-dashboard/script.js
@@ -8,3 +8,16 @@ const students = [
 ];
 
 // Start coding here...
+function displayStudent({ name, grade }) {
+  return `Student ${name} has a grade of ${grade}.`;
+}
+const studentToUpper = students.map(student => student.name.toUpperCase());
+const studentGrade = students.filter(grades => grades.grade >= 85).map(displayStudent);
+const totalOfGrades = students.reduce((acc,student) => acc + (typeof student.grade === "number" ? student.grade : 0),0).toString();
+
+let studentName = document.getElementById("student_name");
+studentName.textContent = studentToUpper.join(" ");
+let studentGradeFilter = document.getElementById("student_GradeFilter");
+studentGradeFilter.textContent = studentGrade.join(" ");
+let studentGradeAvg = document.getElementById("student_Avg");
+studentGradeAvg.textContent = `The Average grades of the students ${totalOfGrades}`;
--- 03-movie-recommender/README.md
+++ 03-movie-recommender/README.md
@@ -4,3 +4,4 @@
 1. Filter movies with rating ≥ 8.
 2. Create a list of movie titles in this format: `"Inception (Sci-Fi) - 8.8 ⭐"`.
 3. Create a function `recommend(movie, minRating = 8)` that returns `true` if rating ≥ minRating.
+
